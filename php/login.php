8d05e107b5c5b8540cffa0f2e1abbaae2839e71e
chuadrian <chuadrian243@gmail.com>
2024-10-01 06:44:44 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,37 +1,36 @@
 <?php
-
 session_start();
-
-    include_once "config.php";
-
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
-
-
-        if(!empty($email) && !empty($password) )
+include_once "config.php";
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+if(!empty($email) && !empty($password))
+{
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+    if(mysqli_num_rows($sql) > 0)
+    {
+        $row = mysqli_fetch_assoc($sql);
+        if(password_verify($password, $row['password']))
         {
-
-                $sql = mysqli_query($conn , "SELECT * FROM users WHERE email = '{$email}' AND password = '{$password}'");
-
-                if(mysqli_num_rows($sql)>0)
-                {
-                    $row = mysqli_fetch_assoc($sql);
-                    $status = "Active now";
-                    $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id ={$row['unique_id']} ");
-                        if($sql2)
-                        {
-                            $_SESSION['unique_id'] = $row['unique_id'];
-                            echo "success";
-                        }
-                }
-                else{
-                    echo "Email or password is incorrect!!";
-                }
-
-        } 
-        else{
-            echo "All input are reuired!!";
+            $status = "Active now";
+            $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+            if($sql2)
+            {
+                $_SESSION['unique_id'] = $row['unique_id'];
+                echo "success";
+            }
         }
-
-
-    ?>
+        else
+        {
+            echo "Email or password is incorrect!";
+        }
+    }
+    else
+    {
+        echo "Email or password is incorrect!";
+    }
+} 
+else
+{
+    echo "All input fields are required!";
+}
+?>
